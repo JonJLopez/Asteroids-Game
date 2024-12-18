b51484dce3fa4221859af73f7b47b45a7dd8b0f8
JonJLopez <jonlop1121@gmail.com>
2024-12-18 12:40:29 -0500

    limit refresh rate to 60fps

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    dt = 0
+    clock = pygame.time.Clock()
+
     while(1):
         #close button functionality
         for event in pygame.event.get():
@@ -16,6 +19,7 @@ def main():
             
         screen.fill(0)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
